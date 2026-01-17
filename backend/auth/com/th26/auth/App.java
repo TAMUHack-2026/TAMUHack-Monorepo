decbcf73e4eb1226f2ab516ff2d088236b5d746e
KedarPanchal <kedar.t.panchal@outlook.com>
2026-01-17 16:33:11 -0600

    Create Spring Boot boilerplate code

---
+++ backend/auth/com/th26/auth/App.java
@@ -0,0 +1,8 @@
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
