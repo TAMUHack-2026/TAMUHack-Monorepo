d9b1f1d85510c1a915fb4b8cc72bab619a6f4c85
KedarPanchal <kedar.t.panchal@outlook.com>
2026-01-19 00:02:55 -0600

    Add better validations for inputs

--- backend/supabase/migrations/schema.sql
+++ backend/supabase/migrations/schema.sql
@@ -4,7 +4,7 @@
 
 CREATE TABLE IF NOT EXISTS users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    email TEXT NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE CHECK(email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z]{2,}$'),
     password_hash TEXT NOT NULL,
 );
 
@@ -13,8 +13,8 @@ CREATE TABLE IF NOT EXISTS profiles (
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     age SMALLINT NOT NULL CHECK (age >= 0 AND age <= 150),
-    sex TEXT NOT NULL CHECK (sex IN ("male", "female")),
+    sex TEXT NOT NULL CHECK (sex IN ('male', 'female')),
     gender_identity TEXT,
     height_in NUMERIC (4, 2) NOT NULL,
-    weight_lb NUMERIC (5, 2) NOT NULL,
+    weight_lbs NUMERIC (5, 2) NOT NULL,
 );
