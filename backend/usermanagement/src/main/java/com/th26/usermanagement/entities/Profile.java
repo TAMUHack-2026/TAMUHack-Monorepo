b1c68a5bbae36701b127563dc67319ab6a113621
KedarPanchal <kedar.t.panchal@outlook.com>
2026-01-23 19:57:45 -0600

    Create toString for entity objects

--- backend/usermanagement/src/main/java/com/th26/usermanagement/entities/Profile.java
+++ backend/usermanagement/src/main/java/com/th26/usermanagement/entities/Profile.java
@@ -20,6 +20,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 import java.util.UUID;
 import java.math.BigDecimal;
@@ -30,6 +31,7 @@ import java.math.BigDecimal;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @Table(name = "profiles")
 public class Profile {
     @Id
