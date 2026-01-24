7455feeef0410daecc7d31ddcfac67ca88134bb9
KedarPanchal <kedar.t.panchal@outlook.com>
2026-01-24 10:11:49 -0600

    Enforce OpenAPI constraints on Pydantic DTO

--- backend/model/dtos/model_input.py
+++ backend/model/dtos/model_input.py
@@ -1,9 +1,10 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, StringConstraints, PositiveFloat
+from typing_extensions import Annotated
 from typing import List
 
 
 class ModelInput(BaseModel):
-    height_in: float
-    weight_lbs: float
-    sex: str
-    breath_data: List[float]
+    height_in: float = Field(..., gt=0, description="Height in inches")
+    weight_lbs: float = Field(..., gt=0, description="Weight in pounds")
+    sex: Annotated[str, StringConstraints(to_lower=True, pattern="^(male|female)$")]
+    breath_data: List[PositiveFloat] = Field(..., min_length=1, description="List of breath data measurements")
