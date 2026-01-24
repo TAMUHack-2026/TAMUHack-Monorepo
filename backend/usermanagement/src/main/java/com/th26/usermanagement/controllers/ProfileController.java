ddf65f8d67ae34b3f90d2a881e4b412171a16b68
KedarPanchal <kedar.t.panchal@outlook.com>
2026-01-23 18:51:46 -0600

    Fix URL parameter parsing in the ProfileController

--- backend/usermanagement/src/main/java/com/th26/usermanagement/controllers/ProfileController.java
+++ backend/usermanagement/src/main/java/com/th26/usermanagement/controllers/ProfileController.java
@@ -5,6 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.th26.usermanagement.dtos.responses.ProfileResponse;
 import com.th26.usermanagement.services.UserService;
@@ -16,7 +17,7 @@ public class ProfileController {
     private UserService userService;
 
     @GetMapping("/{email}")
-    public ResponseEntity<ProfileResponse> getProfileByEmail(String email) {
+    public ResponseEntity<ProfileResponse> getProfileByEmail(@PathVariable("email") String email) {
         return ResponseEntity.ok(userService.getProfileByEmail(email));
     }
 }
