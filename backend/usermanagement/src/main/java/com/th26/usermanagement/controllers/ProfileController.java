391759f948428319d01873387cbdcc8d1ea5ed87
KedarPanchal <kedar.t.panchal@outlook.com>
2026-01-23 18:56:08 -0600

    Add email validation to ProfileController URL parameter

--- backend/usermanagement/src/main/java/com/th26/usermanagement/controllers/ProfileController.java
+++ backend/usermanagement/src/main/java/com/th26/usermanagement/controllers/ProfileController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+
 import com.th26.usermanagement.dtos.responses.ProfileResponse;
 import com.th26.usermanagement.services.UserService;
 
@@ -17,7 +20,7 @@ public class ProfileController {
     private UserService userService;
 
     @GetMapping("/{email}")
-    public ResponseEntity<ProfileResponse> getProfileByEmail(@PathVariable("email") String email) {
+    public ResponseEntity<ProfileResponse> getProfileByEmail(@PathVariable("email") @Email(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Z]{2,}$", flags = Pattern.Flag.CASE_INSENSITIVE) String email) {
         return ResponseEntity.ok(userService.getProfileByEmail(email));
     }
 }
