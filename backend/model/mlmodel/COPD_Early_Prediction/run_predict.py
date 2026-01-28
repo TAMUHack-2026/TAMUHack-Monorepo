0732b35e8a5f54f76b9998ebe1ced2ca8414d2fb
bubba bob10 jha <shreyjha10@gmail.com>
2026-01-28 01:23:22 -0600

    COPD inference testing ready

---
+++ backend/model/mlmodel/COPD_Early_Prediction/run_predict.py
@@ -0,0 +1,67 @@
+import os
+import torch
+
+from utils.predict_utils import (
+    load_spiro_encoder,
+    preprocess_data,
+    run_spiro_encoder,
+    run_spiro_explainer,
+    load_cb_model,
+    run_spiro_predictor,
+)
+
+from utils.predict_utils import (
+    load_spiro_encoder,
+    preprocess_data,
+    run_spiro_encoder,
+    run_spiro_explainer,
+    load_cb_model,
+    run_spiro_predictor,
+)
+
+
+
+
+def copd_risk(data_path: str, age: int, sex: int, smoke: int):
+    folders = ['weights']
+    for folder in folders:
+        if not os.path.exists(folder):
+            os.makedirs(folder)
+
+
+    torch.set_num_threads(8)
+
+    processed_data = preprocess_data(
+        input_path=data_path, age=age, sex=sex, smoke=smoke
+    )
+
+    device = torch.device("cpu") #
+
+    # Run SpiroEncoder
+    spiro_encoder_original_result, attention_weights, all_input_x = run_spiro_encoder(
+        model=load_spiro_encoder(device_str='cuda', model_path="./weights/SpiroEncoder.pth"),
+        data=processed_data,
+        device=device
+    )
+
+    # Run SpiroExplainer
+    spiro_explainer_result, image_base64, spiro_probs= run_spiro_explainer(
+        model=load_cb_model(model_path="./weights/SpiroExplainer.cbm"),
+        data=processed_data,
+        threshold=0.1,
+        spiro_encoder_original_result=spiro_encoder_original_result,
+        attention_weights=attention_weights,
+        all_input_x=all_input_x,
+        is_show=False
+    )
+
+    # Run SpiroPredictor if necessary
+    spiro_predictor_result = {}
+    
+    spiro_predictor = run_spiro_predictor(
+        model=load_cb_model(model_path="./weights/SpiroPredictor.cbm"),
+        data=processed_data
+    )
+    spiro_predictor_result = spiro_predictor[0][1:6]
+    
+    return spiro_explainer_result, spiro_predictor_result, spiro_probs 
--- backend/model/mlmodel/model.py
+++ backend/model/mlmodel/model.py
@@ -1,23 +1,41 @@
 from asthma.inference import AsthmaRiskModel
 from backend.model.mlmodel.asthma_preprocessing import summary_statisitcs
+from COPD_Early_Prediction.run_predict import copd_risk
+import numpy as np
 
 def infer(height, weight, sex, age, breath_data, blow_duration):
     # Dummy implementation for inference
     #sex: 0 is female, 1 is male
-    
+
     asthma_model = AsthmaRiskModel("asthma/asthma_model.joblib")
-        
-    data  = summary_statisitcs()
-    data["age"] = age
-    data["sex"] = 1 #male
-    data["height_cm"] = height
-    data["weight_kg"] = weight
-    data["bmi"] = weight/(height**2)
+
+    asthma_data  = summary_statisitcs()
+    asthma_data["age"] = age
+    asthma_data["sex"] = 1 #male
+    asthma_data["height_cm"] = height
+    asthma_data["weight_kg"] = weight
+    asthma_data["bmi"] = weight/(height**2)
 
     #asthma model output
-    risk =  AsthmaRiskModel.predict_risk(data)["risk"]
+    asthma_risk =  AsthmaRiskModel.predict_risk(asthma_data)["risk"]
 
-    return risk
+    
+    # copd code
+
+    threshold = 0.1
+
+    copd_risk = copd_risk(
+        data_path = "./infer.xlsx",
+        age = age,
+        sex = sex,
+        smoke = np.random.choice([0,1], p=[0.1,0.9]) # 10% of adults smoke roughly
+    )
+
+    copd_prob = copd_risk[2][1]
+    copd_noprob = copd_risk[2][0]
+    copd_class = copd_risk[0]
+    
+    return asthma_risk, copd_class
 
 
 
